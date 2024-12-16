962a63db719e56e5e98d4d604189a87a055fea98
AlessCreator <147744053+Inconnu8557@users.noreply.github.com>
2024-12-16 12:04:54 +0100

    Add files via upload

---
+++ Test/script.js
@@ -0,0 +1,10 @@
+document.querySelector('.hero-content button').addEventListener('mouseover', () => {
+    document.querySelector('.hero-content button').style.transform = 'scale(1.1)';
+});
+
+document.querySelector('.hero-content button').addEventListener('mouseout', () => {
+    document.querySelector('.hero-content button').style.transform = 'scale(1)';
+});
+document.querySelector('.hero-content button').addEventListener('souris', () => {
+    document.querySelector('.hero-content button').style.transform = 'scale(39)';
+});
